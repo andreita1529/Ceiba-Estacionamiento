dde4c64d8495c0d1b9cce7b50ec4b7814a524c20
andreita1529 <andreita1529@gmail.com>
2019-06-28 10:20:42 -0500

    cambios en capa de dominio

--- src/test/java/co/com/ceiba/parqueadero/paola/modelo/unitarias/ParqueaderoTest.java
+++ src/test/java/co/com/ceiba/parqueadero/paola/modelo/unitarias/ParqueaderoTest.java
@@ -3,7 +3,8 @@ package co.com.ceiba.parqueadero.paola.modelo.unitarias;
 import org.junit.Test;
 
 import co.com.ceiba.parqueadero.paola.basetest.BaseTest;
-import co.com.ceiba.parqueadero.paola.dominio.excepcion.ExcepcionValidarPlaca;
+import co.com.ceiba.parqueadero.paola.dominio.constantes.Constantes;
+import co.com.ceiba.parqueadero.paola.dominio.excepcion.*;
 import co.com.ceiba.parquedero.paola.testdatabuilder.ParqueaderoTestDataBuilder;
 
 public class ParqueaderoTest {
@@ -14,7 +15,7 @@ public class ParqueaderoTest {
 		ParqueaderoTestDataBuilder parqueaderoTestDataBuilder = new ParqueaderoTestDataBuilder();
 		parqueaderoTestDataBuilder.placa(null);
 		//Act - Assert
-		BaseTest.assertThrows(() -> parqueaderoTestDataBuilder.build(),ExcepcionValidarPlaca.class,"La placa no puede ser nula.");
+		BaseTest.assertThrows(() -> parqueaderoTestDataBuilder.build(),ExcepcionValidarPlaca.class,Constantes.MENSAJE_PLACA_NULA);
 	}
 	
 	@Test
@@ -23,7 +24,7 @@ public class ParqueaderoTest {
 		ParqueaderoTestDataBuilder parqueaderoTestDataBuilder = new ParqueaderoTestDataBuilder();
 		parqueaderoTestDataBuilder.tipoVehiculo(null);
 		//Act - Assert
-		BaseTest.assertThrows(() -> parqueaderoTestDataBuilder.build(),ExcepcionValidarPlaca.class,"El tipo de vehiculo no puede ser nulo.");
+		BaseTest.assertThrows(() -> parqueaderoTestDataBuilder.build(),ExcepcionTipoVehiculoNulo.class,Constantes.MENSAJE_TIPO_VEHICULO_NULO);
 	}
 	
 	@Test
@@ -32,17 +33,18 @@ public class ParqueaderoTest {
 		ParqueaderoTestDataBuilder parqueaderoTestDataBuilder = new ParqueaderoTestDataBuilder();
 		parqueaderoTestDataBuilder.tipoVehiculo("CARR");
 		//Act - Assert
-		BaseTest.assertThrows(() -> parqueaderoTestDataBuilder.build(),ExcepcionValidarPlaca.class,"El tipo de vehiculo debe ser CARRO o MOTO");
+		BaseTest.assertThrows(() -> parqueaderoTestDataBuilder.build(),ExcepcionTipoVehiculo.class,Constantes.MENSAJE_TIPO_VEHICULO_INCORRECTO);
 	}
 	
 	@Test
 	public void validarCilindrajeObligatorio() {
 		//Arrange
 		ParqueaderoTestDataBuilder parqueaderoTestDataBuilder = new ParqueaderoTestDataBuilder();
+		parqueaderoTestDataBuilder.placa("HIL01E");
 		parqueaderoTestDataBuilder.tipoVehiculo("MOTO");
 		parqueaderoTestDataBuilder.cilindraje(0);
 		//Act - Assert
-		BaseTest.assertThrows(() -> parqueaderoTestDataBuilder.build(),ExcepcionValidarPlaca.class,"Si el tipo de vehiculo es moto debe ingresar el cilindraje");
+		BaseTest.assertThrows(() -> parqueaderoTestDataBuilder.build(),ExcepcionCilindrajeObligatorio.class,Constantes.MENSAJE_CILINDRAJE_NO_NULO);
 	}
 	
 }
--- src/test/java/co/com/ceiba/parqueadero/paola/servicio/unitarias/ServicioParqueaderoTest.java
+++ src/test/java/co/com/ceiba/parqueadero/paola/servicio/unitarias/ServicioParqueaderoTest.java
@@ -4,16 +4,17 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import co.com.ceiba.parqueadero.paola.basetest.BaseTest;
-import co.com.ceiba.parqueadero.paola.dominio.excepcion.ExcepcionRegistroDuplicado;
+import co.com.ceiba.parqueadero.paola.dominio.constantes.Constantes;
+import co.com.ceiba.parqueadero.paola.dominio.excepcion.*;
 import co.com.ceiba.parqueadero.paola.dominio.modelo.Parqueadero;
 import co.com.ceiba.parqueadero.paola.dominio.puerto.repositorio.IParqueaderoRepositorio;
-import co.com.ceiba.parqueadero.paola.dominio.servicio.CrearVehiculoParqueaderoServicio;
+import co.com.ceiba.parqueadero.paola.dominio.servicio.*;
 import co.com.ceiba.parquedero.paola.testdatabuilder.ParqueaderoTestDataBuilder;
 
 public class ServicioParqueaderoTest {
 	
 	@Test
-	public void validarRegistroNoExiste() {
+	public void validarVehiculoNoExiste() {
 		//Arrange
 		Parqueadero parqueadero = new ParqueaderoTestDataBuilder().build();
 		IParqueaderoRepositorio parqueaderoRepositorio = Mockito.mock(IParqueaderoRepositorio.class);
@@ -21,6 +22,19 @@ public class ServicioParqueaderoTest {
 		//Act
 		CrearVehiculoParqueaderoServicio servicioCrearParqueadero = new CrearVehiculoParqueaderoServicio(parqueaderoRepositorio);
 		//Assert
-		BaseTest.assertThrows(() -> servicioCrearParqueadero.crear(parqueadero), ExcepcionRegistroDuplicado.class,"Este Vehiculo ya se encuentra dentro del parqueadero. Por favor revisar");
+		BaseTest.assertThrows(() -> servicioCrearParqueadero.crear(parqueadero), ExcepcionRegistroDuplicado.class,Constantes.VEHICULO_YA_EXISTE_EN_PARQUEADERO);
 	}
+	
+	@Test
+	public void validarExisteActualizar() {
+		//Arrange
+		Parqueadero parqueadero = new ParqueaderoTestDataBuilder().build();
+		IParqueaderoRepositorio parqueaderoRepositorio = Mockito.mock(IParqueaderoRepositorio.class);
+		Mockito.when(parqueaderoRepositorio.existeVehiculo(Mockito.any())).thenReturn(false);
+		//Act
+		ActualizarVehiculoParqueaderoServicio servicioCrearParqueadero = new ActualizarVehiculoParqueaderoServicio(parqueaderoRepositorio);
+		//Assert
+		BaseTest.assertThrows(() -> servicioCrearParqueadero.actualizar(parqueadero), ExcepcionNoSeActualiza.class,Constantes.MENSAJE_NO_SE_PUEDE_ACTUALIZAR);
+	}
+	
 }
